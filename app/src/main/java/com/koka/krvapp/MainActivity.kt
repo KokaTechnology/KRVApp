6aa23a4981cad80e63f1136ff9c76c43762f4834
KokaTechnology <140252958+AJAlex06@users.noreply.github.com>
2023-12-02 11:29:08 +0530

    02.12.23
    Initial Commit

--- app/src/main/java/com/koka/krvapp/MainActivity.kt
+++ app/src/main/java/com/koka/krvapp/MainActivity.kt
@@ -1,32 +1,142 @@
 package com.koka.krvapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.webkit.JavascriptInterface
+import android.webkit.PermissionRequest
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
 
 class MainActivity : AppCompatActivity() {
 
 
-    lateinit var webView : WebView
+    lateinit var webView: WebView
+    private val FILECHOOSER_RESULTCODE = 1
+    private var mUploadMessage: ValueCallback<Array<Uri>>? = null
+    private val mCapturedImageURI: Uri? = null
 
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    @SuppressLint("JavascriptInterface")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         webView = findViewById(R.id.webView)
         webView.webViewClient = WebViewClient()
-        webView.loadUrl("http://210.18.133.181/krv/dashboard.php")
+        webView.loadUrl("https://akashresidency.co.in/krv_developer/")
         webView.settings.javaScriptEnabled = true
         webView.settings.setSupportZoom(true)
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowContentAccess = true
+        webView.settings.safeBrowsingEnabled = true
+        webView.settings.mediaPlaybackRequiresUserGesture = false
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+            }
+        }
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onShowFileChooser(
+                webView: WebView?,
+                filePathCallback: ValueCallback<Array<Uri>>?,
+                fileChooserParams: FileChooserParams?
+            ): Boolean {
+                super.onShowFileChooser(webView, filePathCallback, fileChooserParams)
+
+                mUploadMessage = filePathCallback
+
+                val intent = Intent(Intent.ACTION_GET_CONTENT)
+                intent.addCategory(Intent.CATEGORY_OPENABLE)
+                intent.type = "*/*"
+                startActivityForResult(intent, FILECHOOSER_RESULTCODE)
+
+                return true
+            }
+
+            override fun onPermissionRequest(request: PermissionRequest?) {
+                if (request!!.resources.contains("android.webkit.resource.VIDEO_CAPTURE")) {
+                    if (ActivityCompat.checkSelfPermission(
+                            this@MainActivity,
+                            Manifest.permission.CAMERA
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        ActivityCompat.requestPermissions(
+                            this@MainActivity, arrayOf(
+                                Manifest.permission.CAMERA
+                            ), 101
+                        )
+                    } else {
+                        request.grant(request.resources)
+                    }
+                } else {
+                    super.onPermissionRequest(request);
+                }
+            }
+        }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (null == mUploadMessage || data == null || resultCode != RESULT_OK) {
+                return
+            }
+
+            val result = when {
+                data.dataString != null -> arrayOf(Uri.parse(data.dataString))
+                else -> emptyArray()
+            }
+
+            mUploadMessage?.onReceiveValue(result)
+            mUploadMessage = null
+        }
+    }
 
     override fun onBackPressed() {
         if (webView.canGoBack())
             webView.goBack()
         else
             super.onBackPressed()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkpermission()
+    }
+
+    private fun checkpermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    Manifest.permission.CAMERA
+                ), 101
+            )
+        }
     }
 }
